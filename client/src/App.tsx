65902241c48c007dc31d8b8e7e7beae7701bab8d
Vikms95 <vms1995@hotmail.com>
2022-09-26 19:00:07 +0200

    setup cloud postgre database

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,21 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useFetcher,
+} from 'react-router-dom'
 import { Dashboard, AuthPageWithForm } from './components'
 import { Container } from 'react-bootstrap'
+import { useEffect } from 'react'
 
 export function App() {
+  useEffect(() => {
+    fetch('http://localhost:4000/api/users')
+      .then(res => res.json())
+      .then(data => console.log(data))
+  })
   return (
     <Router>
       <Container fluid className='vh-100 h-100'>
