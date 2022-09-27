1b5a88a4132297ca3c883b429c4b88c9a62483a8
Vikms95 <vms1995@hotmail.com>
2022-09-27 22:56:23 +0200

    fix parser compatibility error

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,15 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Dashboard, AuthPageWithForm } from './components'
 import { Container } from 'react-bootstrap'
-import { useEffect } from 'react'
 
 export function App() {
-  useEffect(() => {
-    fetch('http://localhost:4000/api/users')
-      .then(res => res.json())
-      .then(data => console.log(data))
-  }, [])
-
   return (
     <Router>
       <Container fluid className='vh-100 h-100' data-testid='app'>
