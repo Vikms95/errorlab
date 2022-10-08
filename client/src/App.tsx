c46621c4ece65c173c0f92e720b6293369b55646
Vikms95 <vms1995@hotmail.com>
2022-10-08 05:12:40 +0200

    create ticket preview skeleton

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -12,18 +12,19 @@ import {
 import { ModalWithForm } from 'components/Modal/ModalWithForm'
 import { Container } from 'react-bootstrap'
 import { ProjectPreview } from 'components/Preview/ProjectPreview'
+import { TicketPreview } from 'components/Preview/TicketPreview'
 
 export function App() {
   return (
     <Router>
       <Container fluid className='app h-100 d-flex flex-row' data-testid='app'>
         <Navbar />
-        <Routes>
+        <Routes>          
           <Route path='/' element={<AuthPageWithForm />} />
           <Route path='/dashboard' element={<Dashboard />} />
           <Route path='/project' element={<ProjectWrapper />} />
           <Route path='/ticket' element={<Ticket />} />
-          <Route path='/project-preview' element={<ProjectPreview />} />
+          <Route path='/project-preview' element={<TicketPreview />} />
         </Routes>
       </Container>
       <ModalWithForm />
--- client/src/components/Preview/TicketPreview.tsx
+++ client/src/components/Preview/TicketPreview.tsx
@@ -1,3 +1,40 @@
+import { Preview } from './Preview'
 export function TicketPreview() {
-  return <div>TicketPreview</div>
+  const data = [
+    {
+      title: 'name',
+      developer: 'today',
+      status: 'today',
+      priority: 'tomorrow',
+      date: 'a lot',
+      settings: 'yes',
+    },
+  ]
+
+  return (
+    <Preview>
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Developer</th>
+          <th>Status</th>
+          <th>Priority</th>
+          <th>Date</th>
+          <th>Settings</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map(x => (
+          <tr key='hi'>
+            <td>{x.title}</td>
+            <td>{x.developer}</td>
+            <td>{x.status}</td>
+            <td>{x.priority}</td>
+            <td>{x.date}</td>
+            <td>{x.settings}</td>
+          </tr>
+        ))}
+      </tbody>
+    </Preview>
+  )
 }
