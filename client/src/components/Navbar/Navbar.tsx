20af51435669e5439ff4298abca87ba671fa5d98
Vikms95 <vms1995@hotmail.com>
2022-10-08 03:58:25 +0200

    create logo for the page title

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,7 +1,15 @@
 import { ReactElement } from 'react'
 import { Link } from 'react-router-dom'
 import { FiChevronsRight } from 'react-icons/fi'
-import { FaHome, FaClipboard, FaTicketAlt, FaWarehouse } from 'react-icons/fa'
+import {
+  FaHome,
+  FaClipboard,
+  FaTicketAlt,
+  FaWarehouse,
+  FaCog,
+} from 'react-icons/fa'
+
+import logo from '../../assets/logo.svg'
 
 interface Props {
   children?: ReactElement | ReactElement[]
@@ -12,38 +20,60 @@ export function Navbar({ children }: Props) {
     <nav className='side-navbar'>
       <ul className='inner-navbar'>
         <Link className='nav-link' to='/dashboard'>
-          <div className='nav-item'>
-            <span className='title-text'> Errorlab </span>
-            <FiChevronsRight className='nav-arrow' />
-          </div>
+          <li className='nav-item logo'>
+            <span className='title-text'>
+              {' '}
+              Errorl{''}
+              <span>
+                <img
+                  src={logo}
+                  style={{
+                    width: '22px',
+                    marginLeft: '2px',
+                    marginRight: '-5px',
+                    marginBottom: '5px',
+                  }}
+                  alt=''
+                />
+              </span>
+              {''} b{' '}
+            </span>
+
+            <FiChevronsRight />
+          </li>
         </Link>
         <Link className='nav-link' to='/dashboard'>
-          <div className='nav-item'>
+          <li className='nav-item'>
             <FaHome />
             <span className='link-text'> Dashboard </span>
-          </div>
+          </li>
         </Link>
         <Link className='nav-link' to='/dashboard'>
-          <div className='nav-item'>
+          <li className='nav-item'>
             <FaWarehouse />
             <span className='link-text'> Company </span>
-          </div>
+          </li>
         </Link>
 
         <Link className='nav-link' to='/dashboard'>
-          <div className='nav-item'>
+          <li className='nav-item'>
             <FaClipboard />
             <span className='link-text'> Project </span>
-          </div>
+          </li>
         </Link>
         <Link className='nav-link' to='/dashboard'>
-          <div className='nav-item'>
+          <li className='nav-item'>
             <FaTicketAlt />
             <span className='link-text'> Ticket </span>
-          </div>
+          </li>
         </Link>
-
         {children}
+        <Link className='nav-link settings' to='/dashboard'>
+          <li className='nav-item'>
+            <FaCog />
+            <span className='link-text'> Settings </span>
+          </li>
+        </Link>
       </ul>
     </nav>
   )
